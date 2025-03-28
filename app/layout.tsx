c8fde2d518ffc5a2a5159526349372da940e3f46
Kovalov Oleh <kovalyovoleg@gmail.com>
2025-03-28 21:28:32 +0200

    installed shadcn ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,28 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import type {Metadata} from 'next';
+import {Inter} from 'next/font/google';
 import '@/assets/styles/globals.css';
+import {APP_DESCRIPTION, APP_NAME, SERVER_URL} from '@/lib/constants';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({subsets: ['latin']});
 
 export const metadata: Metadata = {
-  title: 'BeStyled',
-  description: 'Amazing ecommerce platform',
+  title: {
+    template: `%s | Prostore`,
+    default: String(APP_NAME),
+  },
+  description: APP_DESCRIPTION,
+  metadataBase: new URL(String(SERVER_URL)),
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout(
+  {
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
   return (
-    <html lang='en'>
-      <body className={`${inter.className} antialiased`}>{children}</body>
+    <html lang="en">
+    <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
